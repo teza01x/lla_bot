702f9bcdb017d892478ed060ac2b037be30e8abc
Teza01x <110857514+teza01x@users.noreply.github.com>
2024-12-27 15:35:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 
-telegram_token = '6796680111:AAHfMkyRYzSwGl1xJgGc6cWuUA_Wl9x3rH0'
-channel_id = -1002072794174
+telegram_token = ''
+channel_id = -111111
 data_base = os.path.join(os.path.dirname(__file__), 'database.db')
-ref_link = "https://t.me/nudegiftbot?start={}"
+ref_link = "https://t.me/?start={}"
 
 START_MENU = 0
 CHECK_CHANNEL_SUB = 1
